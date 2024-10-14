c7d8a85be23296cc2871f22a9f454a810e426594
vijay27108 <vijay.garg@ltimindtree.com>
2024-10-14 14:51:46 +0530

    code_updated

---
+++ code3.py
@@ -0,0 +1,49 @@
+
+# Different Type of Datatypes
+# integers, floats, booleans, strings, dictionaries,sets, lists , tuples etc.
+# integer datatype
+x =2
+print(type(x))
+# float
+y=2.2
+print(type(y))
+z=22/7
+print(type(z))
+print(z)
+# round to 4 decimals
+print(round(z,2))
+a= 1.0
+print(type(a))
+# Boolean
+z_one = True
+print(z_one)
+print(type(z_one))
+#Boolean Experssion
+print(1<2)
+print(1>2)
+a = (5<3)
+print(a)
+b = not a
+print(b)
+
+
+# Strings
+
+w = "Hello, World"
+print(type(w))
+# To check the length of string
+print(len(w))
+# Empty string
+v = ""
+print("Empty string",v)
+print(type(v))
+
+# Adding two strings
+new_string = "abc"+"def"
+print(new_string)
+print(type(new_string))
+
+# addition and subtraction are not possible 
+# Multipication is possible
+print(3*"abc")
+
