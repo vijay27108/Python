9b6f92028ad6ab069fb2c314bcee65776e5ea8fd
vijay27108 <vijay.garg@ltimindtree.com>
2024-10-27 16:22:17 +0530

    basics

---
+++ Python_Basics.py
@@ -0,0 +1,29 @@
+# To check python version
+# python  --version
+#  To print the line
+print("Hello, World!")
+# We can use '' for printing but not " '
+print('Hello, World!')
+# We can do calculation in the print statement
+print(1+3)
+print(7*6)
+print(8/9) 
+print(8//9) # without decimals (Only Integer)
+
+#  for blank line
+print()
+# we can pass multiple arguments(string and numbers) in print statement
+print("Hello, World !",5)
+# String concatnation
+print("Hello" + " " + "world")
+
+# Pass string to the variables and print
+name = input("Please Enter the name: ")
+last_name = "Sharma"
+# new line character \n
+print("My name is Rahul.\n I am doing job in IT company")
+#  Triple """""" for large paragraph or multiple ''''
+print("""Vinod is working in my dad's factory""")
+# tab
+print("Number 1\tThe Larch")
+print("Number 2\tThe Horse Chestnut")
